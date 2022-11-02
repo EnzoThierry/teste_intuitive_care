0ed2be1459b927f3dc281c716dda325c233a8c24
Enzo Thierry <thierryenzo@protonmail.com>
2022-11-02 16:45:43 -0300

    Update pdf_to_csv.py

--- teste_2/pdf_to_csv.py
+++ teste_2/pdf_to_csv.py
@@ -9,18 +9,10 @@ tables = camelot.read_pdf('./Anexo_1.pdf', process_background=True, pages='3-100
 tables[0].to_csv('./convertido.csv', sep=";")
 
 
-#tables = camelot.read_pdf('./Anexo_1.pdf', pages='4', process_background=True) #Anexo_I > pdf
-#tables.export('Anexo_1.csv', f='csv', compress=True)
-#tables[0].to_csv('./Anexo_1.csv')
-
-
-#def post_process_table(full_table): #incerir legendas
-#    full_table["OD"].replace({ "OD": "Seg. Odontológica" }, inplace=True)
-#    full_table["AMB"].replace({ "AMB": "Seg. Ambulatorial" }, inplace=True)
 
 
 
-"""def zip_file(name, path): #ultima def zip
+def zip_file(name, path): #ultima def zip
     print('Comprimindo dados...')
     handle = zipfile.ZipFile(name, 'w')
     os.chdir(path)
@@ -33,4 +25,4 @@ tables[0].to_csv('./convertido.csv', sep=";")
     #os.makedirs('./files', exist_ok=True)
 
 
-zip_file('Teste_Enzo_Thierry.zip', './files') #'Teste_Intuitive_Care_Enzo_Thierry.zip'"""
+zip_file('Teste_Enzo_Thierry.zip', './files') #'Teste_Intuitive_Care_Enzo_Thierry.zip'
