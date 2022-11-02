1b02e9d09ab20d4373b38bfdb760f35485978aff
Enzo Thierry <thierryenzo@protonmail.com>
2022-11-02 16:44:10 -0300

    upload

--- teste_2/pdf_to_csv.py
+++ teste_2/pdf_to_csv.py
@@ -4,16 +4,14 @@ import zipfile
 import os
 
 
-tables = camelot.read_pdf('./Anexo_1.pdf', pages='4', process_background=True) #Anexo_I > pdf
-tables.export('Anexo_1.csv', f='csv', compress=True)
-tables[0].to_csv('./Anexo_1.csv')
+print('Convertendo PDF para tabelas CSV...')
+tables = camelot.read_pdf('./Anexo_1.pdf', process_background=True, pages='3-100')
+tables[0].to_csv('./convertido.csv', sep=";")
 
 
-#tabelas = [] #pandas formatar legenda
-#for i in range(1, 7):
-#    tabelas.append(tables[i].df)
-#tabelas = pd.concat(tabelas)
-#tabelas.to_csv('C:/Users/Enzo Thierry/Documents/Atari/DOCS/csv/', index=False)
+#tables = camelot.read_pdf('./Anexo_1.pdf', pages='4', process_background=True) #Anexo_I > pdf
+#tables.export('Anexo_1.csv', f='csv', compress=True)
+#tables[0].to_csv('./Anexo_1.csv')
 
 
 #def post_process_table(full_table): #incerir legendas
@@ -22,7 +20,7 @@ tables[0].to_csv('./Anexo_1.csv')
 
 
 
-def zip_file(name, path): #ultima def
+"""def zip_file(name, path): #ultima def zip
     print('Comprimindo dados...')
     handle = zipfile.ZipFile(name, 'w')
     os.chdir(path)
@@ -32,7 +30,7 @@ def zip_file(name, path): #ultima def
             handle.write(x, compress_type= zipfile.ZIP_DEFLATED)
     print('Dados comprimidos!')
     handle.close()
-    os.makedirs('./files', exist_ok=True)
+    #os.makedirs('./files', exist_ok=True)
 
 
-zip_file('Teste_Enzo_Thierry.zip', './files') #'Teste_Intuitive_Care_Enzo_Thierry.zip'
+zip_file('Teste_Enzo_Thierry.zip', './files') #'Teste_Intuitive_Care_Enzo_Thierry.zip'"""
