c5084750b40b6bb82f90327d678046ea0e5461ee
Enzo Thierry <thierryenzo@protonmail.com>
2022-11-05 02:47:27 -0300

    upload

--- teste_2/pdf_to_csv.py
+++ teste_2/pdf_to_csv.py
@@ -1,28 +1,26 @@
 import camelot
 import pandas as pd
 import zipfile
-import os
 
-
+# O codigo le o PDF
 print('Convertendo PDF para tabelas CSV...')
-tables = camelot.read_pdf('./Anexo_1.pdf', process_background=True, pages='3-100')
-tables[0].to_csv('./convertido.csv', sep=";")
+tables = camelot.read_pdf('./Anexo_1.pdf', process_background=True, pages='3-180') #Anexo_1 > pages= 3-180 diz quais as paginas 
+#Tratando os dataframe com Pandas
+table_list = [] 
+for i in range(len(tables)):
+    table_list.append(tables[i].df)
+df = pd.concat(table_list)
+#df.columns = ["PROCEDIMENTO","RN","VIGÊNCIA","OD","AMB","HCO","HSO","REF","PAC","DUT","SUBGRUPO","GRUPO","CAPÍTULO"]
+df.columns = tables[0].df.iloc[0].tolist() #deixa a tabulacao de forma autonoma
+# correcao de legendas
+df['OD'].replace("OD" , "Seg. Odontológica" , inplace=True)
+df['AMB'].replace("AMB" , "Seg. Ambulatorial" , inplace=True)
+df.to_csv('convertido.csv', sep=";" , index=False)
 
+#converte o CSV para .ZIP
+handle = zipfile.ZipFile('./Teste_Enzo_Thierry.zip' , 'w')
+handle.write(f"./convertido.csv", compress_type=zipfile.ZIP_DEFLATED)
+handle.close()
+#tables.export('Teste_Enzo_Thierry.csv', f='csv', compress=True)
 
-
-
-
-def zip_file(name, path): #ultima def zip
-    print('Comprimindo dados...')
-    handle = zipfile.ZipFile(name, 'w')
-    os.chdir(path)
-
-    for x in os.listdir(path):
-        if x.endswith('.csv'):
-            handle.write(x, compress_type= zipfile.ZIP_DEFLATED)
-    print('Dados comprimidos!')
-    handle.close()
-    #os.makedirs('./files', exist_ok=True)
-
-
-zip_file('Teste_Enzo_Thierry.zip', './files') #'Teste_Intuitive_Care_Enzo_Thierry.zip'
+#requirements
